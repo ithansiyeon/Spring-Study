e1c5a56857b9500825598e55bfeb0b5800575977
siyeon <itsiyeon@nate.com>
2023-12-05 23:18:45 +0900

    필드 동기화-개발

--- advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
+++ advanced/src/main/java/hello/advanced/trace/logtrace/FieldLogTrace.java
@@ -1,26 +1,36 @@
-package hello.advanced.trace.hellotrace;
+package hello.advanced.trace.logtrace;
 
 import hello.advanced.trace.TraceId;
 import hello.advanced.trace.TraceStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
-public class HelloTraceV1 {
+public class FieldLogTrace implements LogTrace {
     private static final String START_PREFIX = "-->";
     private static final String COMPLETE_PREFIX = "<--";
     private static final String EX_PREFIX = "<X-";
+    private TraceId traceIdHolder; //traceId 동기화, 동시성 이슈 발생
 
+    @Override
     public TraceStatus begin(String message) {
-        TraceId traceId = new TraceId();
+        syncTraceId();
+        TraceId traceId = traceIdHolder;
         Long startTimeMs = System.currentTimeMillis();
         log.info("[{}] {}{}", traceId.getId(), addSpace(START_PREFIX, traceId.getLevel()), message);
         return new TraceStatus(traceId, startTimeMs, message);
     }
+
+    private void syncTraceId() {
+        if (traceIdHolder == null) traceIdHolder = new TraceId();
+        else traceIdHolder = traceIdHolder.createNextId();
+    }
+
+    @Override
     public void end(TraceStatus status) {
         complete(status, null);
     }
+
+    @Override
     public void exception(TraceStatus status, Exception e) {
         complete(status, e);
     }
@@ -37,7 +47,17 @@ public class HelloTraceV1 {
             log.info("[{}] {}{} time={}ms ex={}", traceId.getId(),
                     addSpace(EX_PREFIX, traceId.getLevel()), status.getMessage(), resultTimeMs,
                     e.toString());
-        } }
+        }
+        releaseTraceId();
+    }
+
+    private void releaseTraceId() {
+        if (traceIdHolder.isFirstLevel()) {
+            traceIdHolder = null; //destory
+        } else {
+            traceIdHolder = traceIdHolder.createPreviousId();
+        }
+    }
 
     //level=0
     //level=1 |-->
@@ -51,5 +71,4 @@ public class HelloTraceV1 {
         }
         return sb.toString();
     }
-
 }
