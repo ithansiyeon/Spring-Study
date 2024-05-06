f13bdd61aa6163a42e81b7d03b5c9e56371ce12b
itsiyeon <itsiyeon@nate.com>
2024-05-06 17:08:55 +0900

    JDK 동적 프록시 - 예제 코드

---
+++ proxy/src/test/java/hello/proxy/jdkdynamic/JdkDynamicProxyTest.java
@@ -0,0 +1,34 @@
+package hello.proxy.jdkdynamic;
+
+import hello.proxy.jdkdynamic.code.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Proxy;
+
+@Slf4j
+public class JdkDynamicProxyTest {
+    @Test
+    void dynamicA() {
+        AInterface target = new AImpl();
+
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+        // handler 프록시가 호출할 로직
+        AInterface proxy = (AInterface) Proxy.newProxyInstance(AInterface.class.getClassLoader(), new Class[]{AInterface.class}, handler);
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+
+    @Test
+    void dynamicB() {
+        BInterface target = new BImpl();
+
+        TimeInvocationHandler handler = new TimeInvocationHandler(target);
+        // handler 프록시가 호출할 로직
+        BInterface proxy = (BInterface) Proxy.newProxyInstance(BInterface.class.getClassLoader(), new Class[]{BInterface.class}, handler);
+        proxy.call();
+        log.info("targetClass={}", target.getClass());
+        log.info("proxyClass={}", proxy.getClass());
+    }
+}
