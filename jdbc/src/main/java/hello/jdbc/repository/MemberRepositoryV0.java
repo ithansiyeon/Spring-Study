4fbf9337083cf8ca72db031cd2abedddb7eb3b17
itsiyeon <itsiyeon@nate.com>
2024-11-04 22:16:22 +0900

    JDBC 개발 - 조회

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 /**
  * JDBC - DriverManger 사용
@@ -27,6 +28,35 @@ public class MemberRepositoryV0 {
 			throw e;
 		} finally {
 			close(con, pstmt, null);
+		}
+	}
+
+	public Member findById(String memberId) throws SQLException {
+		String sql = "select * from MEMBER where member_id = ?";
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			con = getConnection();
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, memberId);
+
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				Member member = new Member();
+				member.setMemberId(rs.getString("member_id"));
+				member.setMoney(rs.getInt("money"));
+				return member;
+			} else {
+				// 운영시에 이런 키 값들 넣어주는게 좋아
+				throw new NoSuchElementException("member not found memberId=" + memberId);
+			}
+		} catch (SQLException e) {
+			log.error("db error", e);
+			throw e;
+		} finally {
+			close(con, pstmt, rs);
 		}
 	}
 
--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV0Test.java
@@ -1,15 +1,28 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
 class MemberRepositoryV0Test {
 	MemberRepositoryV0 repository = new MemberRepositoryV0();
 	@Test
 	void crud() throws SQLException {
+		//save
 		Member member = new Member("memberV1", 10000);
 		repository.save(member);
+
+		//find
+		Member findMember = repository.findById(member.getMemberId());
+		log.info("findMember={}", findMember);
+		log.info("member != findMember {}", member == findMember);
+		// 롬복 쓰면 EqualsAndHashCode 자동으로 make
+		log.info("member equals findMember {}", member.equals(findMember));
+		assertThat(findMember).isEqualTo(member);
 	}
 }
